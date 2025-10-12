cc3f52596d0169621c9eb5238b88bf620642b591
mkg0996 <marcgoellnitz@gmail.com>
2025-10-13 01:15:50 +0200

    Fetshing Games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
@@ -11,7 +15,8 @@ const GenreList = () => {
   //if (isLoading) return <Spinner />;
   return (
     <List>
-      {isLoading && skeletons.map((skeleton) => <GenreListSkeleton />)}
+      {isLoading &&
+        skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
@@ -20,7 +25,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
--- src/components/GenreListSkeleton.tsx
+++ src/components/GenreListSkeleton.tsx
@@ -1,11 +1,13 @@
-import { HStack, ListItem, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GenreListSkeleton = () => {
   return (
-    <ListItem>
+    <>
       <Skeleton height="32px" />
-      <SkeletonText />
-    </ListItem>
+      <HStack>
+        <SkeletonText />
+      </HStack>
+    </>
   );
 };
 
