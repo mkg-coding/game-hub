37a33115d4ddbe8bcc31d53ed29cae1afd81a8d9
mkg-coding <marcgoellnitzcoding@gmail.com>
2025-10-18 23:46:35 +0200

    Fetching Platforms Using React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import parent_platform from "../data/parent_platform";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
@@ -6,10 +9,15 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({
-  data: parent_platform,
-  isLoading: false,
-  error: null,
-}); //useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["parent_platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: parent_platform.length, results: parent_platform },
+  }); //useData<Platform>("/platforms/lists/parents");
 
 export default usePlatforms;
