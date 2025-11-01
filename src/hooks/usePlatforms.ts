aec74ab8a4ce9740f9d669973b8eb3c931a11103
mkg-coding <marcgoellnitzcoding@gmail.com>
2025-11-01 23:07:19 +0100

    Refactoring Entities

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import parent_platform from "../data/parent_platform";
 import APIClient from "../services/api-client";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 const usePlatforms = () =>
